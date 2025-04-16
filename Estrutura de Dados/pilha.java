4a08f02b6f87213137f2077e1e13389aabd691ed
William Santos de Santana <william.santana2@outlook.com>
2025-04-16 00:21:21 +0000

    First Commit

--- Estrutura de Dados/pilha.java
+++ Estrutura de Dados/pilha.java
@@ -1,15 +1,38 @@
-
-import java.util.ArrayList;
+import java.util.Stack;
 
 public class pilha {
-    public static void main(String[] args) {
-        ArrayList<String> pilha = new ArrayList<>();
-        
-        pilha.add("PRATO A");
-        pilha.add("PRATO B");
-        pilha.add("PRATO C");
-        pilha.add("PRATO D");
 
-        System.out.println(pilha);
+    static Stack<String> pilha = new Stack<>();
+
+    static void shift(String prato){
+
+        int lastIndex = pilha.size() - 1;
+
+        if (lastIndex == -1) {
+            pilha.push(prato);
+            return;
+        }
+
+        for (int i = pilha.size() - 1; i >= 0; i--) {
+            if (lastIndex == i) {
+                pilha.push(pilha.get(i));
+            } else {
+                pilha.set(i + 1, pilha.get(i));
+            }
+        }
+
+        pilha.set(0, prato);
+    }
+
+
+    public static void main(String[] args) {
+        shift("PRATO A\n");
+        shift("PRATO B\n");
+        shift("PRATO C\n");
+        shift("PRATO D\n");
     }
 }
+
+
+
+
